8ec1565a858fe25efea633e8dac39670b3f41a25
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2021-04-13 12:58:34 +0200

    Handle error_logger:info_msg/2 the similarly as logger:info/2

--- src/jsonformat.erl
+++ src/jsonformat.erl
@@ -27,6 +27,8 @@
 %%%_* Code =============================================================
 %%%_ * API -------------------------------------------------------------
 -spec format(logger:log_event(), logger:formatter_config()) -> unicode:chardata().
+format(#{msg:={report, #{format:=Format, args:=Args, label:={error_logger, _}}}} = Map, Config) ->
+  format(Map#{msg := {report, #{text => io_lib:format(Format, Args)}}}, Config);
 format(#{level:=Level, msg:={report, Msg}, meta:=Meta}, Config) when is_map(Msg) ->
   encode(pre_encode(maps:merge(Msg, maps:put(level, Level, Meta)), Config));
 format(Map = #{msg := {report, KeyVal}}, Config) when is_list(KeyVal) ->
